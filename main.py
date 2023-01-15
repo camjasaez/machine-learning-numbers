4e7cd0b38b126d45f03b61ecb272d3e7bbce40b1
Camilo Saez <camjasaez@gmail.com>
2023-01-14 21:38:45 -0300

    Version preliminar

---
+++ main.py
@@ -0,0 +1,131 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+
+datos, metadatos = tfds.load(
+    'mnist', as_supervised=True, with_info=True)
+
+datos_entrenamiento, datos_pruebas = datos['train'], datos['test']
+
+nombres_clases = metadatos.features['label'].names
+
+# Hace que la red aprenda mejor y mas rapido
+
+
+def normalizar(imagenes, etiquetas):
+    imagenes = tf.cast(imagenes, tf.float32)
+    imagenes /= 255  # Aqui lo pasa de 0-255 a 0-1
+    return imagenes, etiquetas
+
+
+# Normalizar los datos de entrenamiento y pruebas con la funcion que hicimos
+datos_entrenamiento = datos_entrenamiento.map(normalizar)
+datos_pruebas = datos_pruebas.map(normalizar)
+
+# Agregar a cache (usar memoria en lugar de disco, entrenamiento mas rapido)
+datos_entrenamiento = datos_entrenamiento.cache()
+datos_pruebas = datos_pruebas.cache()
+
+# Crear el modelo
+modelo = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+    tf.keras.layers.Dense(20, activation=tf.nn.relu),
+    tf.keras.layers.Dense(20, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+modelo.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+    metrics=['accuracy']
+)
+
+
+# El trabajo por lotes permite que entrenamientos con gran cantidad de datos se haga de manera mas eficiente
+TAMANO_LOTE = 32
+num_ej_entrenamiento = metadatos.splits["train"].num_examples
+num_ej_pruebas = metadatos.splits["test"].num_examples
+
+# Shuffle y repeat hacen que los datos esten mezclados de manera aleatoria para que la red
+# no se vaya a aprender el orden de las cosas
+datos_entrenamiento = datos_entrenamiento.repeat().shuffle(
+    num_ej_entrenamiento).batch(TAMANO_LOTE)
+datos_pruebas = datos_pruebas.batch(TAMANO_LOTE)
+
+historial = modelo.fit(datos_entrenamiento, epochs=10,
+                       steps_per_epoch=math.ceil(num_ej_entrenamiento/TAMANO_LOTE))
+
+print(str(historial.history["loss"]))
+
+plt.xlabel("Cantidad de epocas")
+plt.ylabel("Magnitud de pérdida")
+plt.plot(historial.history["loss"])
+plt.show()
+
+plt.xlabel("Cantidad de epocas")
+plt.ylabel("Magnitud de precision")
+plt.plot(historial.history["accuracy"])
+plt.show()
+
+# Ahora imprimiremos por pantalla 25 de las imagenes con su respectiva prediccion y porcentaje
+for imagenes_prueba, etiquetas_prueba in datos_pruebas.take(1).cache():
+    imagenes_prueba = imagenes_prueba.numpy()
+    etiquetas_prueba = etiquetas_prueba.numpy()
+    predicciones = modelo.predict(imagenes_prueba)
+
+
+def graficar_imagen(i, arr_predicciones, etiquetas_reales, imagenes):
+    arr_predicciones, etiqueta_real, img = arr_predicciones[i], etiquetas_reales[i], imagenes[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img[..., 0], cmap=plt.cm.binary)
+
+    etiqueta_prediccion = np.argmax(arr_predicciones)
+    if etiqueta_prediccion == etiqueta_real:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(nombres_clases[etiqueta_prediccion],
+                                         100*np.max(arr_predicciones),
+                                         nombres_clases[etiqueta_real]),
+               color=color)
+
+
+def graficar_valor_arreglo(i, arr_predicciones, etiqueta_real):
+    arr_predicciones, etiqueta_real = arr_predicciones[i], etiqueta_real[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    grafica = plt.bar(range(10), arr_predicciones, color="#777777")
+    plt.ylim([0, 1])
+    etiqueta_prediccion = np.argmax(arr_predicciones)
+
+    grafica[etiqueta_prediccion].set_color('red')
+    grafica[etiqueta_real].set_color('blue')
+
+
+filas = 5
+columnas = 5
+num_imagenes = filas*columnas
+plt.figure(figsize=(2*2*columnas, 2*filas))
+for i in range(num_imagenes):
+    plt.subplot(filas, 2*columnas, 2*i+1)
+    graficar_imagen(i, predicciones, etiquetas_prueba, imagenes_prueba)
+    plt.subplot(filas, 2*columnas, 2*i+2)
+    graficar_valor_arreglo(i, predicciones, etiquetas_prueba)
+
+plt.show()
+
+# Probar una imagen suelta
+# AL ser la variable imagenes_prueba solo tiene lo que se le puso en el bloque anterior heheh
+# imagen = imagenes_prueba[4]
+# imagen = np.array([imagen])
+# prediccion = modelo.predict(imagen)
+
+# print("Prediccion: " + nombres_clases[np.argmax(prediccion[0])])
